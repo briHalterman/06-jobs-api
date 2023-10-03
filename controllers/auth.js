5cdd4efcff2966cb617bf353645a40a16f2cf4f7
Brittany Halterman <halterma@gmail.com>
2023-10-03 10:59:07 -0400

    set up authorization controllers structure

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,12 @@
 //  AUTHENTICATION CONTROLLER
+const register = async (req, res) => {
+    res.send('register user');
+};
+const login = async (req, res) => {
+    res.send('login user');
+};
+
+module.exports = {
+    register,
+    login
+};
