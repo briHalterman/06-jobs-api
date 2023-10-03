6f02f1c75bcf474053be25d17d11279d1f3740c8
Brittany Halterman <halterma@gmail.com>
2023-10-03 11:24:29 -0400

    set up authorization routes structure

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,14 @@
 // AUTHETICATION ROUTE
+// set up router
+
+// imports
+const express = require('express');
+const router = express.Router();
+
+const { login, register } = require('../controllers/auth');
+
+// set up routes
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
