765ead8b10233c5152873e2df1831be98b83d8c2
Brittany Halterman <halterma@gmail.com>
2023-10-03 11:08:21 -0400

    set up movies controllers structure

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,1 +1,25 @@
 // MOVIES CONTROLLER
+// get all jobs, get job, create job, update job, delete job --- CRUD functionality
+const getAllUserMovies = async (req, res) => {
+    res.send("get all user's movies");
+};
+const getMovie = async (req, res) => {
+    res.send('get individual movie');
+};
+const createMovie = async (req, res) => {
+    res.send('create movie entry');
+};
+const updateMovie = async (req, res) => {
+    res.send('update movie entry');
+};
+const deleteMovie = async (req, res) => {
+    res.send('delete movie entry');
+};
+
+module.exports = {
+    getAllUserMovies,
+    getMovie,
+    createMovie,
+    updateMovie,
+    deleteMovie
+};
