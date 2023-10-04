cc156424ee8fb0b040aab05a67f83fb7ad31fd51
Brittany Halterman <halterma@gmail.com>
2023-10-04 08:23:53 -0400

    check for empty values in the controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,15 +3,24 @@
 const User = require('../models/User');
 // get status codes
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
     // res.send('register user');
     // res.json(req.body);
+
+    // const { name, email, password } = req.body;
+    // if (!name || !email || !password) {
+    //     throw new BadRequestError("Please provide name, email and password")
+    // }
+
     const user = await User.create({...req.body});
+    // very bad parctice to save passwords as strings as they come in, we will fix that soon
+
     // res.status(StatusCodes.CREATED).json(req.body);
     res.status(StatusCodes.CREATED).json({ user });
-    // very bad parctice to save passwords as strings as they come in, we will fix that soon
 };
+
 const login = async (req, res) => {
     res.send('login user');
 };
