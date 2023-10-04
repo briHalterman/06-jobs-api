da9fef9c6b121f80b5b4acca677a2cd685dab757
Brittany Halterman <halterma@gmail.com>
2023-10-04 08:12:46 -0400

    set up register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 //  AUTHENTICATION CONTROLLER
+// import model
+const User = require('../models/User');
+// get status codes
+const { StatusCodes } = require('http-status-codes');
+
 const register = async (req, res) => {
-    res.send('register user');
+    // res.send('register user');
+    // res.json(req.body);
+    const user = await User.create({...req.body});
+    // res.status(StatusCodes.CREATED).json(req.body);
+    res.status(StatusCodes.CREATED).json({ user });
+    // very bad parctice to save passwords as strings as they come in, we will fix that soon
 };
 const login = async (req, res) => {
     res.send('login user');
