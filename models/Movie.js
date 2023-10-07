802ae1677eef43d44725a1034ad5fe6a929112f0
Brittany Halterman <halterma@gmail.com>
2023-10-07 06:41:07 -0400

    set up movie model

--- models/Movie.js
+++ models/Movie.js
@@ -1,1 +1,56 @@
 // MOVIE MODEL
+// create a movie model
+// set up mongoose
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema({
+    // provide director, title, release year, studio and genre on frontend
+    title: {
+        type: String,
+        required: [true, 'Please provide title.'],
+        maxlength: 100
+    },
+    director: {
+        type: String,
+        required: [true, 'Please provide director name. (If unknown, type "unknown".)'],
+        maxlength: 50
+    },
+    releaseYear: {
+        type: Number,
+        required: [true, 'Please provide release year. (If unknown, type "0000".)']
+    },
+    studio: {
+        type: String,
+        maxlength: 50,
+        required: [true, 'Please provide studio. (If unknown, type "unknown".)']
+    },
+    genre: {
+        type: String,
+        enum: ['Action', 'Adventure', 'Animation (Adult)', 'Animation (Childrens)', 'Animation (Family)', 'Baliwood', 'Biblical', 'Biopic', 'Comedy', 'Crime', 'Cultural Relic', 'Documentary (Biographical)', 'Documentary (Natural)', 'Documentary (Social)', 'Documentary (Other)', 'Drama', 'Educational', 'Family', 'Fantasy', 'Film Noir', 'Historical Fiction', 'Horror', 'Musical Theatre', 'Mystery', 'Performance', 'Religious Values', 'Rock Opera', 'Romance', 'RomCom', 'Sci-Fi', 'Silent Film', 'Sport', 'Superhero', 'Suspense', 'Thriller', 'War', 'Western', 'Other'],
+        required: [true, 'Please select genre.']
+    },
+    // specificGenre: {},
+    favorite: {
+        type: Boolean,
+        default: false
+    },
+    userRating: {
+        type: Number,
+        default: 4.5
+    },
+    // review: {},
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    // most important property - ties movie to actual user
+    // every time a movie is created it is assigned to a user
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        // tie the movie to the user
+        ref: 'User',
+        required: [true, 'Please provide user.']
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Movie', MovieSchema)
