3ad0401d808457c34f5e8d49bf64881416317a9a
Brittany Halterman <halterma@gmail.com>
2023-10-07 20:21:04 -0400

    implement functionality to get all of a user's movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,6 @@
 // MOVIES CONTROLLER
 // only deal with resources as they are associated with a user
-// get all moviess, get indiv. movie, create movie, update movie, delete movie --- CRUD functionality
+// get all moviess, get indiv movie, create movie, update movie, delete movie --- CRUD functionality
 
 // create imports: status codes, movie model, requests (bad request error and not found error)
 const Movie = require('../models/Movie');
@@ -8,7 +8,9 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllUserMovies = async (req, res) => {
-    res.send("get all user's movies");
+    // res.send("get all user's movies");
+    const movies = await Movie.find({ createdBy: req.user.userId }).sort('createdAt');
+    res.status(StatusCodes.OK).json({ movies, count: movies.length });
 };
 const getMovie = async (req, res) => {
     res.send('get individual movie');
