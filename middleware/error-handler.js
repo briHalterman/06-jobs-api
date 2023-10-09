947be55c48b636a40e4bcd737ffa6ad8885047f2
Brittany Halterman <halterma@gmail.com>
2023-10-09 08:59:52 -0400

    handle mongoose validation errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -15,6 +15,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
 
+  if (err === 'ValidationError') {
+    // console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+    customError.statusCode = 400;
+  }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field. Please choose another value.`;
     customError.statusCode = 400;
@@ -26,6 +34,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }); 
   return res.status(customError.statusCode).json({ msg: customError.msg });
 }
-// we will work on errorHandlerMiddleware to check for multiple mongoose errors instead of always sending back 500 with long error message 
+// // we will work on errorHandlerMiddleware to check for multiple mongoose errors instead of always sending back 500 with long error message 
 
 module.exports = errorHandlerMiddleware
