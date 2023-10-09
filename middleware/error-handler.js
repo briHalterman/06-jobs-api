94e5f06519ec0b3c97fa3772ec6dfc29bf9ad23d
Brittany Halterman <halterma@gmail.com>
2023-10-09 07:48:15 -0400

    handle duplicate email error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,14 +2,29 @@
 const { CustomAPIError } = require('../errors') // get custom api error
 const { StatusCodes } = require('http-status-codes') 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // console.log(err);
+
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong. Try again later.'
+  };
+  
   // if custom api error, send the response with the status code and send error message
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field. Please choose another value.`;
+    customError.statusCode = 400;
+  }
+  
   // use status codes library:
   // adds consisteny
   // easier to understand whats happening
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }) 
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }); 
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 }
 // we will work on errorHandlerMiddleware to check for multiple mongoose errors instead of always sending back 500 with long error message 
 
