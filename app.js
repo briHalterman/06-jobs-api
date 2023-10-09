3fa76269fb65a3a03708d08bc6c1b5be675154bb
Brittany Halterman <halterma@gmail.com>
2023-10-09 10:18:12 -0400

    add security protection for deploying the app

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ require('express-async-errors');
 // packages must be used whenever an application is deployed publicly to minimize the chance of a security exposure
 
 // import extra security packages
-
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit'); // will require configuration
 
 const express = require('express'); // look for express
 const app = express(); // invoke express and set equal to app
@@ -44,9 +47,24 @@ const moviesRouter = require('./routes/movies');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1); // because app will be behind reverse proxy
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  })
+  // error message defaults to 'Too many requests, please try again later'
+  // default status code response = 429
+);
+
 // invoke .json because we want to access data in req.body for POST routes
 app.use(express.json());
+
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+// app.use(rateLimter());
 
 // routes
 // app.get('/', (req, res) => { // forward-slash (/) leads to home page 
