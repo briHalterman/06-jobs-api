922d553d342a201c3d08231141a40f04e365069e
Brittany Halterman <halterma@gmail.com>
2023-10-18 16:18:36 -0400

    replace jobs.js with movies.js

--- public/movies.js
+++ public/movies.js
@@ -2,4 +2,4 @@
 
 // front end JavaScript does not run in Node
 // delivered to the browser and runs in the browser context
-//  full access to the document, window, and DOM
+// full access to the document, window, and DOM
