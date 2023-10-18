ee4b7357913b3104b72a2b7a091a6603064a19a0
Brittany Halterman <halterma@gmail.com>
2023-10-18 16:54:35 -0400

    set up movies.js and modify code to match movies data model

--- public/movies.js
+++ public/movies.js
@@ -3,3 +3,43 @@
 // front end JavaScript does not run in Node
 // delivered to the browser and runs in the browser context
 // full access to the document, window, and DOM
+
+// there are various divs and controls to be manipulated by this JavaScript
+// change this code to match movies data model
+// divs and controls must be resolved by their ID
+
+// Start movies.js with the following code:
+document.addEventListener("DOMContentLoaded", () => {
+    const logoff = document.getElementById("logoff");
+    const message = document.getElementById("message");
+    const logonRegister = document.getElementById("logon-register");
+    const logon = document.getElementById("logon");
+    const register = document.getElementById("register");
+    const logonDiv = document.getElementById("logon-div");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const logonButton = document.getElementById("logon-button");
+    const logonCancel = document.getElementById("logon-cancel");
+    const registerDiv = document.getElementById("register-div");
+    const name = document.getElementById("name");
+    const email1 = document.getElementById("email1");
+    const password1 = document.getElementById("password1");
+    const password2 = document.getElementById("password2");
+    const registerButton = document.getElementById("register-button");
+    const registerCancel = document.getElementById("register-cancel");
+    const movies = document.getElementById("movies");
+    const moviesTable = document.getElementById("movies-table");
+    const moviesTableHeader = document.getElementById("movies-table-header");
+    const addMovie = document.getElementById("add-movie");
+    const editMovie = document.getElementById("edit-movie");
+    const title = document.getElementById("title");
+    const director = document.getElementById("director");
+    const releaseYear = document.getElementById("release-year");
+    const studio = document.getElementById("studio");
+    const genre = document.getElementById("genre");
+    const addingMovie = document.getElementById("adding-movie");
+    const moviesMessage = document.getElementById("movies-message");
+    const editCancel = document.getElementById("edit-cancel");
+  
+    // section 2 
+});
